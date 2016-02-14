689c6e2f675210ce285c98436d3ef817f97c2bfd
Gabe Berke-Williams <gabebw@gabebw.com>
2016-02-13 20:32:28 -0800

    Gracefully fail to build a User from Twitter creds
    Instead of using `fromJust` (and possibly crashing), handle the case where a
    User couldn't be created from the OAuth information that Twitter sends back to
    us.

--- Model/User.hs
+++ Model/User.hs
@@ -4,18 +4,20 @@ module Model.User
 
 import Import.NoFoundation
 
-import Data.Maybe (fromJust)
-
 authenticateUser :: AuthId m ~ UserId => Creds m -> DB (AuthenticationResult m)
 authenticateUser Creds{..} = do
-    let twitterUserId = fromJust $ lookup "user_id" credsExtra
-    muser <- getBy $ UniqueUser twitterUserId
+    let mTwitterUserId = (lookup "user_id" credsExtra)
+    muser <- maybe (return Nothing) (getBy . UniqueUser) mTwitterUserId
     case muser of
-      Nothing -> Authenticated <$> insert (credsToUser credsExtra)
-      (Just user) -> return $ Authenticated $ entityKey user
+        Nothing -> createUser (credsToUser credsExtra)
+        (Just user) -> return $ Authenticated $ entityKey user
+    where
+        createUser :: AuthId m ~ UserId => Maybe User -> DB (AuthenticationResult m)
+        createUser (Just user) = Authenticated <$> insert user
+        createUser Nothing = return $ ServerError "Something went wrong"
 
-credsToUser :: [(Text, Text)] -> User
-credsToUser credsExtra = fromJust $ User
+credsToUser :: [(Text, Text)] -> Maybe User
+credsToUser credsExtra = User
     <$> (lookup "user_id" credsExtra)
     <*> (lookup "screen_name" credsExtra)
     <*> (lookup "oauth_token" credsExtra)
