b72d89736d563dd6300ed2ac2d8bd82791f9011b
Gabe Berke-Williams <gabebw@gabebw.com>
2016-02-22 18:10:15 -0800

    Factor out function for today/tomorrow/yesterday

--- src/Croniker/Time.hs
+++ src/Croniker/Time.hs
@@ -12,13 +12,13 @@ import Data.Time.Zones (timeZoneForUTCTime)
 import Data.Time.Zones.All (tzByLabel)
 
 today :: (MonadIO m) => m Day
-today = liftIO getCurrentTime >>= dayM
+today = todayPlus 0
 
 tomorrow :: (MonadIO m) => m Day
-tomorrow = liftIO getCurrentTime >>= dayM . addUTCTime oneDay
+tomorrow = todayPlus 1
 
 yesterday :: (MonadIO m) => m Day
-yesterday = liftIO getCurrentTime >>= dayM . addUTCTime (-1 * oneDay)
+yesterday = todayPlus (-1)
 
 localTime :: UTCTime -> User -> LocalTime
 localTime utcNow user = utcToLocalTime timezone utcNow
@@ -26,8 +26,11 @@ localTime utcNow user = utcToLocalTime timezone utcNow
         timezone = timeZoneForUTCTime tz utcNow
         tz = tzByLabel $ userTzLabel user
 
-dayM :: (MonadIO m) => UTCTime -> m Day
-dayM = return . utctDay
+todayPlus :: (MonadIO m) => NominalDiffTime -> m Day
+todayPlus diff = liftIO getCurrentTime >>= dayM . addUTCTime (oneDay * diff)
+    where
+        dayM :: (MonadIO m) => UTCTime -> m Day
+        dayM = return . utctDay
 
-oneDay :: NominalDiffTime
-oneDay = 60 * 60 * 24
+        oneDay :: NominalDiffTime
+        oneDay = 60 * 60 * 24
