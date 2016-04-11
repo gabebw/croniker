50c722254c686ee14164370777eeb670f401f733
Gabe Berke-Williams <gabebw@gabebw.com>
2016-04-10 23:37:41 -0700

    Use `any` instead of `or . map` to exit early
    Due to Haskell's laziness `or . map` might have exited early anyway, but `any`
    communicates intent better.

--- src/Croniker/UrlParser.hs
+++ src/Croniker/UrlParser.hs
@@ -12,7 +12,7 @@ import Text.Parsec.Text (Parser)
 type Url = String
 
 containsUrl :: T.Text -> Bool
-containsUrl t = or $ map isUrl (T.words $ T.toLower t)
+containsUrl t = any isUrl (T.words $ T.toLower t)
 
 isUrl :: T.Text -> Bool
 isUrl = isRight . parse url ""
