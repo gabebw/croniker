9bab3eb6745c02b39fd70fb0cbfcb17a8ba1a05b
Gabe Berke-Williams <gabebw@gabebw.com>
2016-04-19 20:21:07 -0700

    Use "strip" for removing chars from beginning/end
    Don't use it for removing chars from anywhere in a Text.

--- src/Croniker/MonikerNormalization.hs
+++ src/Croniker/MonikerNormalization.hs
@@ -8,16 +8,16 @@ import Data.Char (ord)
 import qualified Data.Text as T
 
 normalize :: Text -> Text
-normalize = T.strip . stripCharacters . removeInvalidUnicode
+normalize = T.strip . stripCharacters . removeDisallowedUnicode
 
--- Twitter strips out fancy Unicode whitespace characters, and doesn't allow
+-- Twitter removes fancy Unicode whitespace characters, and doesn't allow
 -- anything outside the Basic Multilingual Plane (which ends at U+FEFF).
-removeInvalidUnicode :: Text -> Text
-removeInvalidUnicode = filter (not . bad)
+removeDisallowedUnicode :: Text -> Text
+removeDisallowedUnicode = filter (not . bad)
     where
         bad c = ord c `elem` [0x202A..0x202F] || ord c >= 0xFEFF
 
--- Strip characters that are not allowed at the beginning/end of monikers (but
+-- Remove characters that are not allowed at the beginning/end of monikers (but
 -- are allowed when surrounded by other characters).
 stripCharacters :: Text -> Text
 stripCharacters = T.dropWhile bad . T.dropWhileEnd bad
