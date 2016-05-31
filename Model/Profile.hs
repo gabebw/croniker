9099f4de80c026002b32a08a61f3d1ab0fb25bf6
Gabe Berke-Williams <gabebw@gabebw.com>
2016-05-30 20:03:18 -0700

    Remove unused code

--- Model/Profile.hs
+++ Model/Profile.hs
@@ -1,6 +1,5 @@
 module Model.Profile
     ( futureProfilesFor
-    , profilesFromTodayFor
     , allProfilesForUpdate
     , findProfileFor
     ) where
@@ -16,11 +15,6 @@ futureProfilesFor :: UserId -> DB [Entity Profile]
 futureProfilesFor userId = do
     day <- today
     selectList [ProfileUserId ==. userId, ProfileDate >=. day] [Asc ProfileDate]
-
-profilesFromTodayFor :: UserId -> DB [Entity Profile]
-profilesFromTodayFor userId = do
-    day <- today
-    selectList [ProfileUserId ==. userId, ProfileDate ==. day] [Asc ProfileDate]
 
 -- Due to time zones, we need to find profiles from yesterday/today/tomorrow (in
 -- UTC) because "yesterday"/"tomorrow" in UTC may be "today" in a user's time
