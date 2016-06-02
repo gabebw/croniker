a9750e576d2e6206df0c0483e3ccb9687a380b33
Gabe Berke-Williams <gabebw@gabebw.com>
2016-06-02 15:43:13 -0700

    Remove more redundant parentheses

--- src/Croniker/Types.hs
+++ src/Croniker/Types.hs
@@ -23,7 +23,7 @@ runOauthReader :: MonadIO m => OauthReader a -> OauthCredentials -> m a
 runOauthReader reader creds = liftIO $ runReaderT reader (toOauth1Auth creds)
 
 toOauth1Auth :: OauthCredentials -> W.Auth
-toOauth1Auth (OauthCredentials{oauthKey, oauthSecret, oauthUserKey, oauthUserSecret}) = W.oauth1Auth
+toOauth1Auth OauthCredentials{oauthKey, oauthSecret, oauthUserKey, oauthUserSecret} = W.oauth1Auth
     oauthKey
     oauthSecret
     oauthUserKey
