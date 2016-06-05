38b5954328dd05c5d56b5492f07e2f891ee56ee8
Gabe Berke-Williams <gabebw@gabebw.com>
2016-06-05 13:16:04 -0700

    Ensure that URLs are really URLs
    The previous solution's simple parsing meant that names like `adarsh.tar.gz`
    (which is not a valid URL) was interpreted as a URL.
    
    Now we use the tld library to ensure that only real URLs are parsed as such.
    
    We must add `http://` to each possible URL before parsing it or else
    `parseTLDText` always returns `Nothing`.
    
    Fixes #52.

--- src/Croniker/UrlParser.hs
+++ src/Croniker/UrlParser.hs
@@ -5,26 +5,17 @@ module Croniker.UrlParser
 import Prelude
 import Data.Monoid ((<>))
 import qualified Data.Text as T
-import Data.Either (isRight)
-import Text.Parsec
-import Text.Parsec.Text (Parser)
-
-type Url = String
+import Data.Maybe (isJust)
+import Network.URI.TLD (parseTLDText)
 
 containsUrl :: T.Text -> Bool
-containsUrl = any isUrl . T.words . T.toLower
+containsUrl = any isUrl . T.words
 
 isUrl :: T.Text -> Bool
-isUrl = isRight . parse url ""
+isUrl = isJust . parseTLDText . withHTTP
 
-url :: Parser Url
-url = do
-    a <- many1 alphaNum
-    b <- string "."
-    c <- tld
-    return $ a <> b <> c
-
--- Twitter allows most 2-letter TLDs, but not `.co`
-tld :: Parser String
-tld = try (count 3 alphaNum)
-    <|> string "co"
+withHTTP :: T.Text -> T.Text
+withHTTP t
+  | "http://" `T.isPrefixOf` t = t
+  | "https://" `T.isPrefixOf` t = t
+  | otherwise = "http://" <> t
---
+++ test/Croniker/UrlParserSpec.hs
@@ -0,0 +1,45 @@
+module Croniker.UrlParserSpec
+    ( main
+    , spec
+    ) where
+
+import Prelude
+import Test.Hspec
+
+import Croniker.UrlParser (containsUrl)
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = describe "Croniker.UrlParser" $ do
+    describe "containsUrl" $ do
+        it "returns true for strings that contain URLs" $ do
+            containsUrl "hello from something.com" `shouldBe` True
+
+        it "returns true for strings that contain mixed-case URLs" $ do
+            containsUrl "hello from SOMEthing.CoM" `shouldBe` True
+
+        it "returns true for strings that contain fancy URLs" $ do
+            containsUrl "hello from something.club" `shouldBe` True
+
+        it "returns true for strings that contain fancier URLs" $ do
+            containsUrl "hello from something.co.uk" `shouldBe` True
+
+        it "returns true for strings that are only URLs" $ do
+            containsUrl "something.com" `shouldBe` True
+
+        it "returns true for strings with http://-prefixed URLs" $ do
+            containsUrl "https://something.com" `shouldBe` True
+
+        it "returns true for strings with https://-prefixed URLs" $ do
+            containsUrl "http://something.com" `shouldBe` True
+
+        it "returns false for strings with nothing that looks like a URL" $ do
+            containsUrl "hello" `shouldBe` False
+
+        it "returns false for strings that contain a file extension" $ do
+            containsUrl "hello.tar.gz" `shouldBe` False
+
+        it "returns false for non-URLs that start with 'http'" $ do
+            containsUrl "httphi" `shouldBe` False
