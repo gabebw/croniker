7b38a1508a207277a55512c2538efbfbfd8b6674
Gabe Berke-Williams <gabebw@gabebw.com>
2016-06-11 14:55:09 -0700

    Use helpful functions from Data.Text.Encoding
    So useful!
    
    According to the docs:
    
        decodeLatin1 is semantically equivalent to
        Data.Text.pack . Data.ByteString.Char8.unpack

--- Helper/TextConversion.hs
+++ Helper/TextConversion.hs
@@ -7,9 +7,10 @@ import Prelude
 
 import qualified Data.ByteString.Char8 as BSC
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 
 b2t :: BSC.ByteString -> T.Text
-b2t = T.pack . BSC.unpack
+b2t = T.decodeLatin1
 
 t2b :: T.Text -> BSC.ByteString
-t2b = BSC.pack . T.unpack
+t2b = T.encodeUtf8
