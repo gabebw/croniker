e3b0a776a0527d0e4ab728f169f0e811bdf8e182
Gabe Berke-Williams <gabebw@gabebw.com>
2016-06-12 14:39:14 -0700

    Extract some well-named functions

--- src/Croniker/UrlParser.hs
+++ src/Croniker/UrlParser.hs
@@ -12,7 +12,10 @@ containsUrl :: T.Text -> Bool
 containsUrl = any isUrl . T.words
 
 isUrl :: T.Text -> Bool
-isUrl url = "." `T.isInfixOf` url && (isJust $ parseTLDText $ withHTTP url)
+isUrl url = hasDot && parsesAsURI
+    where
+        hasDot = "." `T.isInfixOf` url
+        parsesAsURI = isJust $ parseTLDText $ withHTTP url
 
 withHTTP :: T.Text -> T.Text
 withHTTP t
