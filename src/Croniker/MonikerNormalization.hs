b4bd29802570d719e6b0d4a54a1bbd9795a5ffea
Gabe Berke-Williams <gabebw@gabebw.com>
2016-06-23 00:07:45 -0700

    Don't remove Unicode/non-Unicode separately
    Really, isn't everything Unicode when you think about it?
    
    Now instead of `removeDisallowedUnicode` and `removeDisallowedCharacters`,
    there's only `removeDisallowedCharacters`, which removes all characters that are
    not allowed anywhere in a moniker.

--- src/Croniker/MonikerNormalization.hs
+++ src/Croniker/MonikerNormalization.hs
@@ -8,20 +8,22 @@ import Data.Char (ord)
 import qualified Data.Text as T
 
 normalize :: Text -> Text
-normalize = T.strip . stripCharacters . removeDisallowedCharacters . removeDisallowedUnicode
-
--- Twitter removes fancy Unicode whitespace characters, and doesn't allow
--- anything outside the Basic Multilingual Plane (which ends at U+FEFF).
-removeDisallowedUnicode :: Text -> Text
-removeDisallowedUnicode = filter (not . bad)
-    where
-        bad c = ord c `elem` [0x202A..0x202F] || ord c >= 0xFEFF
+normalize = T.strip . stripCharacters . removeDisallowedCharacters
 
 -- Remove characters that aren't allowed anywhere in monikers.
 removeDisallowedCharacters :: Text -> Text
-removeDisallowedCharacters = T.filter (not . bad)
+removeDisallowedCharacters = filter (not . bad)
     where
-        bad c = c `elem` ['<', '>']
+        bad c = c `elem` ['<', '>'] || fancyUnicodeWhitespace c || outsideBMP c
+
+-- Is this character outside Unicode's Basic Multilingual Plane (which ends at
+-- U+FEFF)?
+outsideBMP :: Char -> Bool
+outsideBMP c = ord c >= 0xFEFF
+
+-- Is this character fancy Unicode whitespace?
+fancyUnicodeWhitespace :: Char -> Bool
+fancyUnicodeWhitespace c = ord c `elem` [0x202A..0x202F]
 
 -- Remove characters that are not allowed at the beginning/end of monikers (but
 -- are allowed when surrounded by other characters).
