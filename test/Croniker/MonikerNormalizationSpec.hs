c0945cbce316e6890cc1fcc623c966663286bba4
Gabe Berke-Williams <gabebw@gabebw.com>
2016-08-22 19:40:34 -0700

    Refactor normalization texts to be more readable
    * Introduce `codepoint2text` to make it clearer how Unicode characters are used

--- test/Croniker/MonikerNormalizationSpec.hs
+++ test/Croniker/MonikerNormalizationSpec.hs
@@ -15,38 +15,42 @@ import qualified Data.Text as T
 main :: IO ()
 main = hspec spec
 
+-- Helpful for stuff like U+267B.
+-- codepoint 0x267B gives you a Text that contains U+267B.
+codepoint2text :: Int -> T.Text
+codepoint2text codepoint = T.singleton $ chr codepoint
+
 spec :: Spec
 spec = describe "Croniker.MonikerNormalization" $ do
     describe "normalize" $ do
         forM_ [0x2028, 0x2029] $ \c -> do
             it (printf "strips U+%X from the beginning of the moniker" c) $ do
-                let t = T.pack $ chr c:"hello"
+                let t = codepoint2text c `T.append` "hello"
                 normalize t `shouldBe` "hello"
 
             it (printf "strips U+%X from the end of the moniker" c) $ do
-                let t = T.pack $ "hello" ++ [chr c]
+                let t = "hello" `T.append` codepoint2text c
                 normalize t `shouldBe` "hello"
 
             it (printf "does not strip U+%X from the middle of the moniker" c) $ do
-                let t = T.pack $ concat ["he", [chr c], "llo"]
-                let good = T.concat ["he", T.singleton $ chr c, "llo"]
-                normalize t `shouldBe` good
+                let t = T.concat ["he", codepoint2text c, "llo"]
+                normalize t `shouldBe` t
 
         forM_ [0x202A..0x202F] $ \c -> do
             it (printf "strips U+%X from the beginning of the moniker" c) $ do
-                let t = T.pack $ chr c:"hello"
+                let t = codepoint2text c `T.append` "hello"
                 normalize t `shouldBe` "hello"
 
             it (printf "strips U+%X from the end of the moniker" c) $ do
-                let t = T.pack $ "hello" ++ [chr c]
+                let t = "hello" `T.append` codepoint2text c
                 normalize t `shouldBe` "hello"
 
             it (printf "strips U+%X from the middle of the moniker" c) $ do
-                let t = T.pack $ concat ["he", [chr c], "llo"]
+                let t = T.concat ["he", codepoint2text c, "llo"]
                 normalize t `shouldBe` "hello"
 
         it "strips chars above U+FEFF from the moniker" $ do
-            let astral_plane_char = T.singleton $ chr 0x10000
+            let astral_plane_char = codepoint2text 0x10000
 
             let t = T.concat [
                         astral_plane_char,
