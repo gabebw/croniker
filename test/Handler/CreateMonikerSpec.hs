9675c051c674c7efb79d162f47eae13814e74530
Gabe Berke-Williams <gabebw@gabebw.com>
2016-08-27 16:25:30 -0700

    at least it compiles

--- test/Handler/CreateMonikerSpec.hs
+++ test/Handler/CreateMonikerSpec.hs
@@ -5,6 +5,9 @@ module Handler.CreateMonikerSpec
 
 import TestImport
 
+import Network.Wai.Test (simpleBody)
+import Text.XML.Cursor hiding (element)
+
 import Data.Time.Zones.All (TZLabel(..))
 
 main :: IO ()
@@ -22,13 +25,20 @@ spec = withApp $ do
                 get RootR
                 void followRedirect
                 postForm ProfileR $ do
-                    byLabel "Moniker" "  \n\t test \n\t  "
+                    byLabel "Moniker" " twitter "
 
-                htmlAnyContain "item[value='test']"
-                -- items `shouldHaveLength` 0
-                -- bodyContains "gabebw"
-                -- htmlAnyContain "input" "Site 1"
-                -- bodyContains "Defaults to the next available date"
+                withResponse $ \response -> do
+                    let body = simpleBody response
+                    htmlQuery "input[id=hident2][value= twitter ]"
+
+                    -- let input = parseHTML body $// attributeIs "id" "hident2" >=> attribute "value"
+                    -- let input = " twitter "
+                    -- shouldBe input " twitter "
+                    -- htmlCount "input#hident2" 1
+                    -- items `shouldHaveLength` 0
+                    -- bodyContains "gabebw"
+                    -- htmlAnyContain "input" "Site 1"
+                    -- bodyContains "Defaults to the next available date"
 
 buildUser :: User
 buildUser = User "1" "gabebw" "token123" "secret123" Etc__UTC True True
