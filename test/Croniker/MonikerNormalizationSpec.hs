d83d22b49900ed905341717ad2fc57a551dec098
Gabe Berke-Williams <gabebw@gabebw.com>
2016-08-27 14:40:10 -0700

    Refactor spec to use ClassyPrelude

--- test/Croniker/MonikerNormalizationSpec.hs
+++ test/Croniker/MonikerNormalizationSpec.hs
@@ -3,36 +3,35 @@ module Croniker.MonikerNormalizationSpec
     , spec
     ) where
 
-import Prelude
+import ClassyPrelude
 import Test.Hspec
 
-import Control.Monad (forM_)
+-- import Control.Monad (forM_)
 import Croniker.MonikerNormalization (normalize)
 import Data.Char (chr)
 import Text.Printf (printf)
-import qualified Data.Text as T
 
 main :: IO ()
 main = hspec spec
 
 -- Helpful for stuff like U+267B.
--- codepoint 0x267B gives you a Text that contains U+267B.
-codepoint2text :: Int -> T.Text
-codepoint2text codepoint = T.singleton $ chr codepoint
+-- `codepoint2text 0x267B` returns a Text that contains U+267B.
+codepoint2text :: Int -> Text
+codepoint2text codepoint = singleton $ chr codepoint
 
 spec :: Spec
 spec = describe "Croniker.MonikerNormalization" $ do
     describe "normalize" $ do
         it "allows codepoints below U+FFFF" $ do
             let recyclingSymbol = codepoint2text 0x267B
-            let t = recyclingSymbol `T.append` "hello"
+            let t = recyclingSymbol `mappend` "hello"
 
             normalize t `shouldBe` t
 
         it "allows codepoints above U+FFFF" $ do
             let astral_plane_char = codepoint2text 0x10000
 
-            let t = T.concat [
+            let t = concat [
                         astral_plane_char,
                         "he",
                         astral_plane_char,
@@ -43,28 +42,28 @@ spec = describe "Croniker.MonikerNormalization" $ do
 
         forM_ [0x2028, 0x2029] $ \c -> do
             it (printf "strips U+%X from the beginning of the moniker" c) $ do
-                let t = codepoint2text c `T.append` "hello"
+                let t = codepoint2text c `mappend` "hello"
                 normalize t `shouldBe` "hello"
 
             it (printf "strips U+%X from the end of the moniker" c) $ do
-                let t = "hello" `T.append` codepoint2text c
+                let t = "hello" `mappend` codepoint2text c
                 normalize t `shouldBe` "hello"
 
             it (printf "does not strip U+%X from the middle of the moniker" c) $ do
-                let t = T.concat ["he", codepoint2text c, "llo"]
+                let t = concat ["he", codepoint2text c, "llo"]
                 normalize t `shouldBe` t
 
         forM_ [0x202A..0x202F] $ \c -> do
             it (printf "strips U+%X from the beginning of the moniker" c) $ do
-                let t = codepoint2text c `T.append` "hello"
+                let t = codepoint2text c `mappend` "hello"
                 normalize t `shouldBe` "hello"
 
             it (printf "strips U+%X from the end of the moniker" c) $ do
-                let t = "hello" `T.append` codepoint2text c
+                let t = "hello" `mappend` codepoint2text c
                 normalize t `shouldBe` "hello"
 
             it (printf "strips U+%X from the middle of the moniker" c) $ do
-                let t = T.concat ["he", codepoint2text c, "llo"]
+                let t = concat ["he", codepoint2text c, "llo"]
                 normalize t `shouldBe` "hello"
 
         it "removes leading whitespace from the moniker" $ do
